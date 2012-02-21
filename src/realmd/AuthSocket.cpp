972506b9f63d570745206f099f819ad2a868af4d
Chglove <chglove@live.cn>
2012-02-21 13:00:07 +0800

    Add 3.3.5b build 13685, 13930 support. * Also maps support for build 13685 and 13930.
    Signed-off-by: Chglove <chglove@live.cn>

--- src/realmd/AuthSocket.cpp
+++ src/realmd/AuthSocket.cpp
@@ -299,6 +299,8 @@ void AuthSocket::SendProof(Sha1Hash sha)
         case 11403:                                         // 3.3.2
         case 11723:                                         // 3.3.3a
         case 12340:                                         // 3.3.5a
+        case 13685:                                         // 3.3.5a
+        case 13930:                                         // 3.3.5b
         default:                                            // or later
         {
             sAuthLogonProof_S proof;
@@ -962,6 +964,8 @@ void AuthSocket::LoadRealmlist(ByteBuffer &pkt, uint32 acctid)
         case 11403:                                         // 3.3.2
         case 11723:                                         // 3.3.3a
         case 12340:                                         // 3.3.5a
+        case 13685:                                         // 3.3.5a
+        case 13930:                                         // 3.3.5b
         default:                                            // and later
         {
             pkt << uint32(0);                               // unused value
--- src/realmd/RealmList.cpp
+++ src/realmd/RealmList.cpp
@@ -36,7 +36,9 @@ extern DatabaseType LoginDatabase;
 // list sorted from high to low build and first build used as low bound for accepted by default range (any > it will accepted by realmd at least)
 
 static RealmBuildInfo ExpectedRealmdClientBuilds[] = {
-    {12340, 3, 3, 5, 'a'},                                  // highest supported build, also auto accept all above for simplify future supported builds testing
+    {13930, 3, 3, 5, 'b'},                                  // highest supported build, also auto accept all above for simplify future supported builds testing
+    {13685, 3, 3, 3, 'a'},
+    {12340, 3, 3, 5, 'a'},
     {11723, 3, 3, 3, 'a'},
     {11403, 3, 3, 2, ' '},
     {11159, 3, 3, 0, 'a'},
